01825b600fe4a3e5917d9377460f6a43b0be758f
Simona Zozuľaková <szozulakova@gmail.com>
2022-11-21 15:19:42 +0100

    getting questions done/gettinf right answear done

--- public/quiz.js
+++ public/quiz.js
@@ -1,1 +1,57 @@
 "use strict";
+const createQuestion = (data) => {
+  const body = document.querySelector("body");
+  const wrapper = document.createElement("div");
+  wrapper.className = "wrapper";
+
+  const questionTitle = document.createElement("h2");
+  questionTitle.className = "questionTitle";
+  questionTitle.innerText = data.question;
+  wrapper.appendChild(questionTitle);
+
+  for (let i = 0; i < 4; i++) {
+    const answer = document.createElement("li");
+    answer.className = "answer";
+    answer.innerText = data.answers[i].answer;
+    answer.dataset.id = data.answers[i].id;
+
+    wrapper.appendChild(answer);
+  }
+
+  const manageLink = document.createElement("a");
+  manageLink.href = "/manageLink";
+  manageLink.innerText = "Manage Link";
+  wrapper.appendChild(manageLink);
+
+  body.appendChild(wrapper);
+};
+const checkAnswer = (data) => {
+  const answers = document.querySelectorAll(".answer");
+  const dataAnswers = data.answers;
+
+  answers.forEach((answer) => {
+    answer.addEventListener("click", (e) => {
+      dataAnswers.forEach((dataAnswer) => {
+        if (answer.dataset.id == dataAnswer.id) {
+          if (dataAnswer.is_correct) {
+            const rightAnswer = document.querySelector(
+              `[data-id="${answer.dataset.id}"]`
+            );
+            rightAnswer.style.backgroundColor = "green";
+          }
+        }
+      });
+    });
+  });
+};
+
+//---------------------------------------------------------
+async function apiHandler() {
+  const data = await fetch("/api/game")
+    .then((res) => res.json())
+    .catch((err) => err);
+
+  createQuestion(data);
+  checkAnswer(data);
+}
+apiHandler();
--- server.js
+++ server.js
@@ -3,13 +3,68 @@ var express = require("express");
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const path = require("path");
 const port = 3000;
-
 app.use(express.static("public"));
-//res.sendFile(__dirname + "/index.html");
+//--------------------------------express set up
+let mysql = require("mysql");
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+let conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "quiz_app",
+});
+conn.connect((err) => {
+  if (err) {
+    console.error("Cannot connect to the database", err);
+    return;
+  }
+  console.log("Connection established");
+});
+//--------------------------------mysql set up
+
+app.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/game", (req, res) => {
+  let randomId = Math.floor(Math.random() * (11 - 1) + 1);
+  const questionResponse = new Promise((res, rej) => {
+    const getQuestion = `SELECT * FROM quiz_app.questions  WHERE id =${randomId}`;
+    conn.query(getQuestion, (err, rows) => {
+      if (err) rej(err);
+      res(rows[0]);
+    });
+  });
+
+  const answersResponse = new Promise((res, rej) => {
+    const getAnswers = `SELECT * FROM quiz_app.answers WHERE question_id=${randomId}`;
+    conn.query(getAnswers, (err, rows) => {
+      if (err) rej(err);
+      res(rows);
+    });
+  });
+
+  const apiHandler = async () => {
+    const question = await questionResponse
+      .then((response) => response)
+      .catch((err) => console.log(err));
+
+    const answers = await answersResponse
+      .then((response) => response)
+      .catch((err) => console.log(err));
+
+    res.json({ ...question, answers: answers });
+  };
+  apiHandler();
+});
+app.get("/api/questions", (req, res) => {
+  const getQuestion = `SELECT * FROM quiz_app.questions`;
+  conn.query(getQuestion, (err, rows) => {
+    if (err) rej(err);
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
